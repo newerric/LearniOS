2a63771b6d4d8ad3fdb5669841f9b38430e0470d
newerric <newerric@yahoo.com>
2019-05-19 21:44:02 +0800

    update UIButton for closure

--- UIKit/UIButton/UIButton/ViewController.swift
+++ UIKit/UIButton/UIButton/ViewController.swift
@@ -9,30 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // how to make system style button via code
+    // clouse {}() ＝> 代表立即執行
+    let codeButton:UIButton = {
         let newButton = UIButton(type: UIButton.ButtonType.system)
         newButton.frame = CGRect(x: 50, y: 50, width: 100, height: 50)
         newButton.setTitle("Press", for: UIControl.State.normal)
         newButton.setTitle("Pressing", for: UIControl.State.highlighted)
-        view.addSubview(newButton)
         newButton.addTarget(self, action: #selector(ViewController.hitMe(_:)), for: UIControl.Event.touchUpInside)
-        
-        // how to make image button via code
+        return newButton
+    }()
+    let anotherButton:UIButton = {
         let anotherButton = UIButton(type: UIButton.ButtonType.custom)
         anotherButton.frame = CGRect(x: 200, y: 200, width: 114, height: 54)
         anotherButton.setImage(UIImage(named: "PlayButton"), for: UIControl.State.normal)
         anotherButton.setImage(UIImage(named: "PlayButtonPressed"), for: UIControl.State.highlighted)
-        view.addSubview(anotherButton)
         anotherButton.addTarget(self, action: #selector(hitMe(_:)), for: UIControl.Event.touchUpInside)
-        
-        
+        return anotherButton
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         print("Hello World")
+    }
+    
+    func setupUI(){
+        view.addSubview(codeButton)
+        view.addSubview(anotherButton)
     }
     
     @objc func hitMe(_ thisButton:UIButton){
