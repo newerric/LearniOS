521fa5aadaf19212e9eab034e631894b3f5cdf9a
newerric <newerric@yahoo.com>
2019-06-20 21:50:36 +0800

    add NewSet

---
+++ UIKit/NewSet/NewSet/NewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewViewController.swift
+//  NewSet
+//
+//  Created by mikewang on 2019/6/20.
+//  Copyright © 2019 mike. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
---
+++ UIKit/NewSet/NewSet/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  NewSet
+//
+//  Created by mikewang on 2019/6/4.
+//  Copyright © 2019 mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
+    }
+ 
+
+}
+
+class AnotherViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .blue
+    }
+}
+
+class ThirdViewController: UIViewController {
+    
+}
