5bf8eff0e13a4919b651eae30f6f392b644c9b76
newerric <newerric@yahoo.com>
2019-07-01 11:50:33 +0800

    add SwitchingViews

---
+++ UIKit/SwitchingViews/SwitchingViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwitchingViews
+//
+//  Created by mikewang on 2019/7/1.
+//  Copyright © 2019 mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
