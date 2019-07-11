7f990941cc0da971f32e6111543c927f395a33a6
newerric <newerric@yahoo.com>
2019-07-11 18:21:08 +0800

    add CollectionView

---
+++ UIKit2/CollectionView/CollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by mikewang on 2019/7/11.
+//  Copyright © 2019 mike. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    
+    
+    
+}
---
+++ UIKit2/CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by mikewang on 2019/7/11.
+//  Copyright © 2019 mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    let images = ["cat1","cat2","cat3","cat4","cat5","cat6","cat7","cat8","cat9","cat10","cat11","cat12","cat13","cat14","cat15"]
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myCollectionView.dataSource = self
+        myCollectionView.delegate = self
+    }
+    
+    // MARK: UICollectionView
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? MyCollectionViewCell else { return MyCollectionViewCell() }
+        cell.myImageView.image = UIImage(named: images[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 3) / 4
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+
+
+}
+
